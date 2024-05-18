d4c85228a25f95314ee52fc580ff3686d14478b7
Shivmohan <nshivmohan08@gmail.com>
2024-05-18 19:40:30 +0530

    drt

--- demo-tls-rest-client/src/main/java/com/tls/client/demotlsrestclient/bean/Beans.java
+++ demo-tls-rest-client/src/main/java/com/tls/client/demotlsrestclient/bean/Beans.java
@@ -1,11 +1,13 @@
 package com.tls.client.demotlsrestclient.bean;
-
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.web.client.RestTemplate;
-
-import javax.net.ssl.SSLContext;
 import java.io.FileInputStream;
 import java.security.KeyStore;
+import javax.net.ssl.SSLContext;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContexts;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
 
 public class Beans {
     public RestTemplate createRestTemplate() throws Exception {
