3440a387348167acc68ecd8b1d98c085ffc71f0b
Shivmohan <nshivmohan08@gmail.com>
2024-05-18 18:49:43 +0530

    SSL/TLS cert config init

---
+++ src/main/java/com/tls/rest/DemoTlsRestApplication.java
@@ -0,0 +1,13 @@
+package com.tls.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTlsRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoTlsRestApplication.class, args);
+	}
+
+}
